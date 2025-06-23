bf32f229dbe59c925c189d0493c551e366f5caea
Alii0319 <165925992+Alii0319@users.noreply.github.com>
2025-06-23 14:01:38 +0500

    admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Record
 # Register your models here.
-admin.site.register(Record)
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display=['id','first_name','last_name','email','phone','address','city','state','zipcode']
